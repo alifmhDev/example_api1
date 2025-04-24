e1455b09c699e4d0015199366995e0d2fd044c56
Qarslan <programmerqarslan@gmail.com>
2025-04-25 02:12:31 +0700

    init project api vercel

--- api/index.js
+++ api/index.js
@@ -2,21 +2,8 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'Halo dari API Express di Vercel!' });
+app.get('/api', (req, res) => {
+  res.json({ message: 'Halo dari API kamu sendiri!' });
 });
-
-router.get('/about', (req, res) => {
-  res.json({ message: 'Ini adalah endpoint /about' });
-});
-
-router.get('/user/:name', (req, res) => {
-  const name = req.params.name;
-  res.json({ message: `Hai ${name}, ini dari dynamic route!` });
-});
-
-app.use('/api', router);
 
 module.exports.handler = serverless(app);
